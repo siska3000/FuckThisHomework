e76bcb66540a8904cc350732a2e18470ef77e68f
Sanya <333.gmail.com>
2025-02-21 12:14:37 +0200

    Added functionality to work woth Excel offline

--- Processors/FilmsProcessor.py
+++ Processors/FilmsProcessor.py
@@ -5,5 +5,5 @@ from Processors.EntityProcesor import EntityProcessor
 class FilmsProcessor(EntityProcessor):
     def process(self, json_data: list) -> pd.DataFrame:
         df = pd.DataFrame(json_data)
-        df['release_year'] = pd.to_datetime(df['release_date']).dt.year  # Додавання нового стовпця
+        df['release_year'] = pd.to_datetime(df['release_date']).dt.year
         return df
--- main.py
+++ main.py
@@ -2,39 +2,55 @@ import argparse
 import ast
 import json
 
+from ExcelSWAPIClient import ExcelSWAPIClient
 from FetchClass import SWAPIClient
 from Processors.FilmsProcessor import FilmsProcessor
 from Processors.PeopleProcessor import PeopleProcessor
 from Processors.PlanetsProcessor import PlanetsProcessor
 from SWAPIDataManager import SWAPIDataManager
 
-client = SWAPIClient(base_url="https://swapi.dev/api/")
+
+parser = argparse.ArgumentParser(description='My simple echo')
+parser.add_argument('--source', '-s', help='Source of data (api or excel)', default='api')
+parser.add_argument('--endpoint', '-e', help='Endpoints to fetch', default='people,planets')
+parser.add_argument('--filters', '-f', help='Filters for data', default='{"people": ["films", "species"], "planets": ["films", "residents"]}')
+parser.add_argument('--output', '-o', help='Output Excel file', default='swapi_data.xlsx')
+
+args = parser.parse_args()
+endpoints = args.endpoint.split(',')
+
+if args.source == "api":
+    client = SWAPIClient(base_url="https://swapi.dev/api/")
+elif args.source == "excel":
+    client = ExcelSWAPIClient(file_path=r"C:\Users\Lenovo\Desktop\BonBinBon.ods")
+else:
+    raise ValueError("Invalid source. Use 'api' or 'excel'")
+
+
 manager = SWAPIDataManager(client)
+
 
 manager.register_processor("people", PeopleProcessor())
 manager.register_processor("planets", PlanetsProcessor())
 manager.register_processor("films", FilmsProcessor())
 
-parser = argparse.ArgumentParser(description='My simple echo')
-parser.add_argument('--endpoint', '-e', help='Text before', default='people,planets')
-parser.add_argument('--filters', '-f', help='Text after', default='{"people": ["films", "species"], "planets": ["films", "residents"]}')
-parser.add_argument('--output', '-o', help='Text after', default='swapi_data.xlsx')
-
-args = parser.parse_args()
-endpoints = args.endpoint.split(',')
-
 try:
     some_dict = ast.literal_eval(args.filters)
     print("Parsed dictionary:", some_dict)
+
     for endpoint in endpoints:
         manager.fetch_entity(endpoint)
-        manager.apply_filter(endpoint, some_dict[endpoint])
+        if endpoint in some_dict:
+            manager.apply_filter(endpoint, some_dict[endpoint])
+
     manager.save_to_excel(args.output)
+
 except json.JSONDecodeError as e:
     print("Invalid JSON format:", e)
-
+except Exception as e:
+    print(f"Error: {e}")
 
 """
 Example of using:
-py main.py --endpoint people,planets --filters "{'people': ['films', 'species'], 'planets': ['films', 'residents']}" --output swapi_data.xlsx
+py main.py --source api --endpoint people,planets --filters "{'people': ['films', 'species'], 'planets': ['films', 'residents']}" --output swapi_data.xlsx
 """
